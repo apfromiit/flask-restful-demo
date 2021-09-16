af5dd968cc53b57f34e3f271288f69b4b015ab83
aptopcoder <aptopcoder@gmail.com>
2021-09-17 01:07:00 +0530

    enable cors

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class HelloWorld1(Resource):
